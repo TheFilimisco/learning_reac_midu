3e59b8501ee746f31e43b36e58378924ffcb4a5f
Filimisco <82726794+StephenDC-UwU@users.noreply.github.com>
2025-10-17 12:26:15 +0200

    add dynamic form

--- projects/hello-world/src/App.jsx
+++ projects/hello-world/src/App.jsx
@@ -5,9 +5,19 @@ export function App () {
     const formatUserName = (userName) => `@${userName}`
     const [getName, setName] = useState('TheFilimisco');
 
+    const users = [
+        {userName: 'TheFilimisco', name: 'Filimisco', isFollowing: true},
+        {userName: 'thelucho', name: 'Lucho', isFollowing: false},
+        {userName: 'janedoe', name: 'Jane Doe', isFollowing: true},
+        {userName: 'johndoe', name: 'John Doe', isFollowing: false}
+    ]
+
     return (
         <section className="App">
-             <TwitterFollowCard formatUserName={formatUserName} userName={getName} name={"TheFilimisco"} isFollowing >
+
+            {/* Manual Form */}
+
+            <TwitterFollowCard formatUserName={formatUserName} userName={getName} name={"TheFilimisco"} initialIsFollowing >
                 {/* Here you can find  <strong>{children}</strong>  */}
                 <span>TheFilimisco</span>
              </TwitterFollowCard>
@@ -19,6 +29,28 @@ export function App () {
             <button onClick={() => setName('NuevoNombre')}>
                 Change Lucho's Name to NewName
             </button>
+
+            {/* Dynamic Form */}
+            {
+            users.map(({userName, name, isFollowing}) => (              
+                    <TwitterFollowCard
+                        key={userName} /* Something never repeat */
+                        formatUserName={formatUserName} 
+                        userName={userName} 
+                        name={name} 
+                        initialIsFollowing={isFollowing}
+                    >
+                        <span>{name}</span>
+                    </TwitterFollowCard>
+                )
+            )
+            }
+
+            <button onClick={() => setName('NuevoNombre')}>
+                Change Lucho's Name to NewName
+            </button>
+
         </section>
     )
 }   
+
--- projects/hello-world/src/TwitterFollowCard.jsx
+++ projects/hello-world/src/TwitterFollowCard.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
 // This a code declarative
-export function TwitterFollowCard ({formatUserName, children, userName, name}) {
-    const [isFollowing, setIsFollowing] = useState(false);
-    const text = isFollowing ? 'Following' : 'Follow';
-    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+export function TwitterFollowCard ({formatUserName, children, userName, name, initialIsFollowing}) {
+
+    //You can use props to set the initial state
+    //And initialize the state only one time on execution
+    //Children prop is used to pass content between the opening and closing tags of a component
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
 
     //Props are read-only (immutable) 
     console.log(isFollowing);
@@ -21,15 +24,17 @@ export function TwitterFollowCard ({formatUserName, children, userName, name}) {
                 className='tw-followCard-avatar' 
                 src={imageSrc} alt="" />
                 <div className='tw-followCard-info'>
-                    <strong>{children}</strong>
+                    <strong>{name}</strong>
                     <span
                     className='tw-followed-infoUserName'
                     >{formatUserName(userName)}</span>
                 </div>
             </header>
             <aside>
-                <button className={buttonClassName} onClick={handleClick}>
-                    <span>{text}</span> {/*This a children of button Dad*/}
+                <button  
+                className={isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'}
+                onClick={handleClick}>
+                    <span>{isFollowing ? 'Following' : 'Follow'}</span> {/*This a children of button Dad*/}
                 </button>
             </aside>
         </article>
